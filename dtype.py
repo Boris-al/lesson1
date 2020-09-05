3f873df071ba6e27b62c9a5e4d5767c6f7925e93
Boris-al <lyubavin.boris@gmail.com>
2020-09-05 21:50:08 +0300

    test

---
+++ dtype.py
@@ -0,0 +1,40 @@
+'''a = 'joHHHn   pem perem 333'.lower().replace('h', 't').capitalize()
+b = a.split()
+print(a.split())
+print(len(b))
+'''
+
+'''a = None
+b = 0'''
+'''print(a is None)
+print(b is not None)
+print(type(a))
+print(type(b))'''
+'''
+name = int(input('enter smth '))
+print(name)
+print(type(name))'''
+
+#v = input('Введите число от 1 до 10: ')
+#n = int(v) + 10
+#print(str(n)) 
+# поправьте код, чтобы выводилось число
+# на 10 больше, чем введённое
+# например, пользователь ввел 10 
+# программа вывела 20
+
+
+#name = input('Введите ваше имя: ').upper()
+#print('Привет, ' + name + '! Как дела?') 
+# поправьте код, чтобы выводилась строка
+# Привет, ИМЯ! Как дела?
+
+
+#print(float('1'))  # ???
+#print(int('2'))  # ???
+#print(bool(1))  # ???
+#print(bool(' '))  # ???
+#print(bool(0))  # ???
+
+name = "Boris"
+print(name)
---
+++ hello.py
@@ -0,0 +1,4 @@
+print('hello world!')
+print('привет программист!')
+print(2+2)
+print(10/3)
